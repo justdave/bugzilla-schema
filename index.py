032108516e7aac43f106a64d20ecd51d8f06d435
Richard Brooksby <rb@ravenbrook.com>
2013-05-22 09:23:16 +0100

    Adding "Fork me on GitHub" ribbon with link.

--- index.py
+++ index.py
@@ -136,6 +136,10 @@ class webpage:
         print '</head>'
         print ('<body bgcolor="#FFFFFF" text="#000000" link="#000099" '
                'vlink="#660066" alink="#FF0000">')
+        print ('<a href="https://github.com/Ravenbrook/bugzilla-schema">'
+               '<img style="position: absolute; top: 0; right: 0; border: 0;"'
+               'src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"'
+               'alt="Fork me on GitHub"></a>')
         print '<div align="center">'
         self.print_directory_links()
         print '<hr />'
